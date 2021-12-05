50f543beadb3daa7bcc35439c0133d700b3f6dfe
mohiu <mohiuddintarek2017@gmail.com>
2021-12-06 02:05:17 +0600

    auth phone signup

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,12 @@ void main() async {
     home: MyApp(),
   ));
 }
-class MyApp extends StatefulWidget{
-  _State createState()=> _State();
+
+class MyApp extends StatefulWidget {
+  _State createState() => _State();
 }
-class _State extends State<MyApp>{
 
-
+class _State extends State<MyApp> {
   /*@override
   void initState() {
     FirebaseAuth.instance
@@ -42,5 +42,33 @@ class _State extends State<MyApp>{
     return Scaffold(
       body: PhoneSignUp(),
     );
+  }
+}
+
+class Initializer extends StatefulWidget {
+  @override
+  _InitializerState createState() => _InitializerState();
+}
+
+class _InitializerState extends State<Initializer> {
+  late FirebaseAuth firebaseAuth;
+  late User _user;
+  bool isloading = true;
+
+  @override
+  void initState() {
+    firebaseAuth = FirebaseAuth.instance;
+    _user = firebaseAuth.currentUser!;
+    this.isloading = false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return this.isloading?Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    ):_user ==null ?PhoneSignUp():HomePage();
   }
 }
