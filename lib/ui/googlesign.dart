e3e63e3c07f7719bde436066bc925bf377d9f6ab
mohiu <mohiuddintarek2017@gmail.com>
2021-12-11 18:46:18 +0600

    facebbok auth

--- lib/ui/googlesign.dart
+++ lib/ui/googlesign.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import 'home.dart';
@@ -16,34 +17,71 @@ class _GoogleSignState extends State<GoogleSign> {
   bool isloading = false;
   Future<UserCredential> signInWithGoogle() async {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-    // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-    // Create a new credential
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken.token);
+
+    // Once signed in, return the UserCredential
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            signInWithGoogle().whenComplete(() {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) {
-                    return HomePage();
-                  },
+        child: Column(
+          children: [
+            Container(
+              height: 30,
+              child: Card(
+                elevation: 4,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: GestureDetector(
+                        onTap: (){
+                          signInWithGoogle().whenComplete(() {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return HomePage();
+                                },
+                              ),
+                            );
+                          });
+                        },
+                          child: Icon(Icons.ac_unit)),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: GestureDetector(
+                        onTap: (){
+                          signInWithFacebook().whenComplete((){
+                            setState(() {
+                              this.isloading = false;
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));
+                            });
+                          });
+                        },
+                          child: Icon(Icons.ac_unit)),
+                    )
+                  ],
                 ),
-              );
-            });
-          },
-          child: Text('Google Sign In'),
-        ),
+              ),
+            )
+          ],
+        )
       ),
     );
   }
